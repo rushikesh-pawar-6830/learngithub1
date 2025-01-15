97f1940866ee8043f876bdc3c9c20c6538a6231d
Rushikesh Pawar <pawarrushikesh02018@gmail.com>
2025-01-15 15:33:03 +0530

    xyz

--- app.py
+++ app.py
@@ -11,3 +11,5 @@ print("HAPPY ARMY DAY")
 print("hello how are you")
 print("jai hind")
 print ("the variety that we see in the living thing theat the exist on the aearth is called as a biodiversity or a biological diversity")
+
+print ("the variety that awe see in the living thing that the exist on the earth is ")
