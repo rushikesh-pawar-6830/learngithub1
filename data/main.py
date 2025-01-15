cf509d33380da3905f3f589177296ab3bed5820b
Rushikesh Pawar <pawarrushikesh02018@gmail.com>
2025-01-15 18:44:53 +0530

    new feat added

--- data/main.py
+++ data/main.py
@@ -1,1 +1,14 @@
 print("hello salman")
+
+print("Rahuri-->pune")
+
+name="rushikesh bharat pawar"
+
+def nameof(name):
+    print("hellooo,how are you feeling",name)
+    
+    
+    
+    print(100+200)
+    print("new feature add feat-b ")
+    
